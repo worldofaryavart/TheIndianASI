1a717524172fa49da37afb5d622fd7149333dbf6
Shiva Saini <104831069+worldofaryavart@users.noreply.github.com>
2025-01-29 12:31:27 +0530

    project section update

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -1,28 +1,41 @@
 import { FaGithub } from "react-icons/fa";
 
 type Project = {
-    id: number;
-    title: string;
-    description: string;
-    link: string;
+  id: number;
+  title: string;
+  description: string;
+  stack: string[];
+  link: string;
 };
 
 const ProjectCard = ({ project }: { project: Project }) => {
-    return (
-        <div className="project-card bg-transparent border border-gray-400 rounded-lg p-6 hover:shadow-lg transition-shadow duration-300">
-            <h3 className="text-xl font-semibold mb-2 text-white">{project.title}</h3>
-            <p className="text-gray-300 mb-4">{project.description}</p>
-            <a
-                href={project.link}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="inline-flex items-center text-blue-400 hover:text-blue-300 transition-colors duration-200"
+  return (
+    <div className="project-card bg-transparent border border-gray-400 rounded-lg p-6 hover:shadow-lg transition-shadow duration-300">
+      <h3 className="text-xl font-semibold mb-2 text-white">{project.title}</h3>
+      <p className="text-gray-300 mb-4">{project.description}</p>
+      <div className="flex justify-center mb-2">
+        <div className="flex flex-wrap gap-2">
+          {project.stack.map((tech, index) => (
+            <span
+              key={index}
+              className="px-3 py-1 text-sm font-medium text-white bg-indigo-600 rounded-full shadow-md"
             >
-                <FaGithub className="mr-2" />
-                <span>View on GitHub</span>
-            </a>
+              {tech}
+            </span>
+          ))}
         </div>
-    );
+      </div>
+      <a
+        href={project.link}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="inline-flex items-center text-blue-400 hover:text-blue-300 transition-colors duration-200"
+      >
+        <FaGithub className="mr-2" />
+        <span>View on GitHub</span>
+      </a>
+    </div>
+  );
 };
 
 export default ProjectCard;
--- src/components/Projects/ProjectSection.tsx
+++ src/components/Projects/ProjectSection.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Typography, Container } from "@mui/material";
 import ProjectCard from "./ProjectCard";
 
@@ -5,21 +7,24 @@ type Project = {
   id: number;
   title: string;
   description: string;
+  stack: string[];
   link: string;
 };
 
 const projects: Project[] = [
   {
     id: 1,
-    title: "Project 1",
-    description: "Description of Project 1",
-    link: "www.github.com/worldofaryavart",
+    title: "India's AI Community",
+    description: "We’re building a global AI & deep tech hub—by India, for the world. No matter your background, you have an equal opportunity to collaborate, innovate, and shape the future.No owners, only builders. Join us now!",
+    stack: ["Nextjs", "React", "TypeScript", "Python"],
+    link: "https://github.com/worldofaryavart/IndiaAI",
   },
   {
-    id: 2, // Changed to a unique id
-    title: "Project 2",
-    description: "Description of Project 2",
-    link: "www.github.com/worldofaryavart",
+    id: 2,
+    title: "LLMForIndia - Open Source AI for Bharat",
+    description: "LLMForIndia is a community-driven initiative to build an open-source language model tailored for Indian languages and knowledge. Our goal is to develop AI that understands and represents India's diverse linguistic and cultural heritage.",
+    stack: ["LLM", "Python", "Open Source"],
+    link: "https://github.com/worldofaryavart/AI-for-India/blob/main/README.md",
   },
 ];
 
