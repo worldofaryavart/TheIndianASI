0ce0e54e4bd09a53313905f81cb3c0d166d8c5e3
Shiva Saini <104831069+worldofaryavart@users.noreply.github.com>
2025-01-29 15:01:36 +0530

    Update CommunityNote.tsx

--- src/components/CommunityNote.tsx
+++ src/components/CommunityNote.tsx
@@ -1,5 +1,10 @@
 import { Container } from "@mui/material";
-import { FaHardHat, FaQuestionCircle, FaHandsHelping } from "react-icons/fa";
+import {
+  FaHardHat,
+  FaQuestionCircle,
+  FaHandsHelping,
+  FaReddit,
+} from "react-icons/fa";
 
 const CommunityNote = () => {
   return (
@@ -34,20 +39,33 @@ const CommunityNote = () => {
         <div className="flex items-start">
           <FaHandsHelping className="text-green-400 text-xl mr-2 flex-shrink-0" />
           <p className="text-gray-300">
-            Some of the data and components, like topics, participant numbers and messages, are not
-            correct and are for demonstration purposes only.
+            Some of the data and components, like topics, participant numbers
+            and messages, are not correct and are for demonstration purposes
+            only.
           </p>
         </div>
 
         {/* Call-to-Action Button */}
-        <div className="mt-6">
-          <a
-            href="https://github.com/worldofaryavart/IndiaAI" // Replace with the actual link or action
-            className="inline-flex items-center px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors duration-200"
-          >
-            <FaHandsHelping className="mr-2" />
-            <span>Collaborate with Us</span>
-          </a>
+        <div className="flex mx-2 gap-2">
+          <div className="mt-6">
+            <a
+              href="https://github.com/worldofaryavart/IndiaAI" // Replace with the actual link or action
+              className="inline-flex items-center px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors duration-200"
+            >
+              <FaHandsHelping className="mr-2" />
+              <span>Collaborate with Us</span>
+            </a>
+          </div>
+
+          <div className="mt-6">
+            <a // Replace with the actual link or action
+              href="https://www.reddit.com/r/TheIndianASI/" // Replace with the actual link or action
+              className="inline-flex items-center px-4 py-2 bg-orange-600 text-white rounded-lg hover:bg-orange-700 transition-colors duration-200"
+            >
+              <FaReddit className="mr-2" />
+              <span>Join Us on Reddit</span>
+            </a>
+          </div>
         </div>
       </div>
     </Container>
