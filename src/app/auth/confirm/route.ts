50f2881b113b565071e3dc337567d7e1b2df8810
Shiva Saini <104831069+worldofaryavart@users.noreply.github.com>
2025-06-18 16:41:11 +0530

    minor fixes

--- src/app/auth/confirm/route.ts
+++ src/app/auth/confirm/route.ts
@@ -1,14 +1,13 @@
 import { type EmailOtpType } from '@supabase/supabase-js'
 import { type NextRequest } from 'next/server'
 
-import { createClient } from '@/utils/supabase/server'
 import { redirect } from 'next/navigation'
+import { createClient } from '@/app/utils/supabase/server'
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url)
   const token_hash = searchParams.get('token_hash')
   const type = searchParams.get('type') as EmailOtpType | null
-  const next = searchParams.get('next') ?? '/'
 
   if (token_hash && type) {
     const supabase = await createClient()
