27990e58111bf64d007196f1a56c41921e10b55f
Shiva Saini <104831069+worldofaryavart@users.noreply.github.com>
2025-06-19 19:01:20 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,15 +3,21 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   optimizeFonts: true,
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: 'https',
-  //       hostname: 'api.dicebear.com',
-  //       pathname: '/7.x/**',
-  //     },
-  //   ],
-  // },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.dicebear.com',
+        pathname: '/7.x/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
